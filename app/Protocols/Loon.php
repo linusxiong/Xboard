a476d4e0215c01d61909bc66629536c797843775
Linus Xiong <siyuanxiong2333@gmail.com>
2025-05-27 11:05:46 -0400

    feat(provider): add 2022-blake3 support

--- app/Protocols/Loon.php
+++ app/Protocols/Loon.php
@@ -27,7 +27,9 @@ class Loon
                     'aes-128-gcm',
                     'aes-192-gcm',
                     'aes-256-gcm',
-                    'chacha20-ietf-poly1305'
+                    'chacha20-ietf-poly1305',
+                    '2022-blake3-aes-128-gcm',
+                    '2022-blake3-aes-256-gcm'
                 ])
             ) {
                 $uri .= self::buildShadowsocks($item['password'], $item);
